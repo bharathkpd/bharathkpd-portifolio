5948ff04bafa5377b4a63044ca16fef252002bc3
kalav <you@example.com>
2026-06-21 15:53:48 +0530

    chore: configure dynamic base path for Netlify and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const isNetlify = process.env.NETLIFY === 'true';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/bharathkpd-portifolio/',
+  base: isNetlify ? '/' : '/bharathkpd-portifolio/',
 })
